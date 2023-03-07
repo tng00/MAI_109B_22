2a2731a7d28149958e75abed618c260e10361c1b
tng00 <tnguen00@gmail.com>
2023-03-07 23:03:37 +0300

    removed using temporary file

--- Huan/lab21/lab21.sh
+++ Huan/lab21/lab21.sh
@@ -14,15 +14,16 @@ fi
 
 dir=$1
 
-# Поиск имён файлов по заданному шаблону, получение списка уникальных отсортированных суффиксов
-find "$dir" -type f -exec basename {} \; | grep -E ".+\..+" | rev | cut -d . -f 1 | rev | sort | uniq > out
-
-suffixes=$(wc -l < out)      # Подсчёт количества суффиксов
-
 echo "Уникальные суффиксы файлов в каталоге $dir:"
-while read line; do
-	echo ".$line"
-done < out
+
+suffixes=0
+
+# Поиск имён файлов по заданному шаблону, получение списка уникальных отсортированных суффиксов
+for file in $(find "$dir" -type f -exec basename {} \; | grep -E ".+\..+" | rev | cut -d . -f 1 | rev | sort | uniq) 
+	do
+	echo ".$file"
+	suffixes=$(($suffixes+1))
+	done
 
 echo "Всего: $suffixes шт."
-rm out
+
