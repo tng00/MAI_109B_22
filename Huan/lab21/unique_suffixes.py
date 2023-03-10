5f279a360a4423e717b7619a1e48f2115b469ef1
tng00 <80770913+tng00@users.noreply.github.com>
2023-03-10 22:51:09 +0300

    removed excess comments

--- Huan/lab21/unique_suffixes.py
+++ Huan/lab21/unique_suffixes.py
@@ -9,19 +9,19 @@ def FindSuffixes(path):
     pattern = r".+\..+$"
     for root, dirs, files in os.walk(path):
         for file in files:
-            if re.match(pattern, file):  # Поиск имён файлов по заданному шаблону
+            if re.match(pattern, file):
                 suffix = "." + file.split(".")[-1]
                 suffixes.add(suffix)
     return suffixes
 
 
 def main():
-    if len(sys.argv) != 2:                # Проверка количества параметров
+    if len(sys.argv) != 2:
         print("Ошибка: необходимо передать один аргумент - путь к директории")
         print("Пример: ./script.py path/to/directory")
         sys.exit(1)
 
-    if not os.path.isdir(sys.argv[1]):    # Проверка, является ли аргумент директорией
+    if not os.path.isdir(sys.argv[1]):
         print(f'Ошибка: "{sys.argv[1]}" не является директорией')
         print("Правильный формат ввода: path/to/directory")
         sys.exit(1)
