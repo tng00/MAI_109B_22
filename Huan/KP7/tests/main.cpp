4b0ea9d491b97437543b034c7bd41bd69423e6be
tng00 <80770913+tng00@users.noreply.github.com>
2023-05-04 00:36:41 +0300

    New interface main.cpp

--- Huan/KP7/tests/main.cpp
+++ Huan/KP7/tests/main.cpp
@@ -4,57 +4,37 @@
 
 
 int main() {
-  bool exit = false, transposed = false;
+  bool exit = false;
+  int action;
+  Matrix matrix;
   while (true) {
     if (exit) {
       break;
     }
     std::cout << "Choose action:\n1 - input matrix\n2 - print usual form\n3 - print schematic form\n"
                  "4 - transpose and check skew symmetric\n5 - benchmark\n6 - exit\n\n";
-    int action;
     std::cin >> action;
     std::string file_name;
     switch (action) {
-      case 1:std::cout << "Enter file name in format: \"in\" + digit 1 to 6. For example: in4.\n";
+      case 1:std::cout << "Enter file name in format: \"in\" + digit from 1 to 6. For example: in4.\n";
         std::cin >> file_name;
-        if (!inputMatrix(file_name)) {
-          std::cout << "Wrong file name!\n\n";
+        if (!matrix.inputMatrix(file_name)) {
+          std::cout << "Wrong file name! Choose 1 and try again.\n\n";
         }
-        A_transposed.clear();
-        M_transposed.clear();
-        transposed = false;
         break;
       case 2:
-        if (M.empty()) {
-          std::cout << "Input the matrix first!\n\n";
-          break;
-        }
-        printUsual();
+        matrix.printUsual();
         break;
       case 3:
-        if (M.empty()) {
-          std::cout << "Input the matrix first!\n\n";
-          break;
-        }
-        printSchematic();
+        matrix.printSchematic();
         break;
       case 4:
-        if (M.empty()) {
-          std::cout << "Input the matrix first!\n\n";
-          break;
-        }
-        checkMatrix(transposed);
-        transposed = true;
+        matrix.checkMatrix();
         break;
       case 5:
         benchmark();
         break;
       case 6:
-        A.clear();
-        M.clear();
-        A_transposed.clear();
-        M_transposed.clear();
-        transposed = false;
         exit = true;
         std::cout << "See you next time!\n\n";
         break;
