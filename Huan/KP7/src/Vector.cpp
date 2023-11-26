713f672a6514093825908c735f3880c317ed6a9a
tng00 <80770913+tng00@users.noreply.github.com>
2023-11-27 02:50:05 +0300

    Update Vector.cpp

--- Huan/KP7/src/Vector.cpp
+++ Huan/KP7/src/Vector.cpp
@@ -1,283 +1,238 @@
-#include "../include/Vector.hpp"
+#include "../include/vector.hpp"
 
-template<class T, class Allocator>
-Vector<T, Allocator>::Vector() : size_(0), capacity_(0) {
-  data_ = allocator_.allocate(capacity());
+template <class T, class Allocator>
+Vector<T, Allocator>::Vector(size_t count, const T &value) : size_(count) {
+    capacity_ = size_ * 2;
+    data_ = allocator_.allocate(capacity_);
+    std::fill_n(data_, count, value);
 }
 
-template<class T, class Allocator>
-Vector<T, Allocator>::Vector(size_t count, const T &value) : size_(count), capacity_(0) {
-  capacity_ = calculateCapacity(count);
-  data_ = allocator_.allocate(capacity());
-  std::fill_n(data(), count, value);
-}
-
-template<class T, class Allocator>
+template <class T, class Allocator>
 Vector<T, Allocator>::Vector(const Vector &other) {
-  this->resize(other.size());
-  capacity_ = other.capacity();
-  std::copy(other.begin(), other.end(), data());
+    size_ = other.size();
+    capacity_ = other.capacity();
+    data_ = allocator_.allocate(capacity_);
+    std::copy(other.begin(), other.end(), data_);
 }
 
-template<class T, class Allocator>
+template <class T, class Allocator>
 Vector<T, Allocator>::Vector(Vector &&other) noexcept {
-  std::swap(size_, other.size_);
-  std::swap(capacity_, other.capacity_);
-  std::swap(data_, other.data_);
-  other.clear();
+    size_ = other.size();
+    capacity_ = other.capacity();
+    data_ = other.data_;
+    other.data_ = nullptr;
 }
 
-template<class T, class Allocator>
-Vector<T, Allocator>::Vector(const std::initializer_list<T>& init) {
-  size_ = init.size();
-  capacity_ = calculateCapacity(size());
-  data_ = allocator_.allocate(capacity());
-  std::copy(init.begin(), init.end(), data());
+template <class T, class Allocator>
+Vector<T, Allocator> &Vector<T, Allocator>::operator=(const Vector &other) {
+    if (*this == other) {
+        return *this;
+    }
+    clear();
+    reallocate(other.capacity());
+    size_ = other.size();
+    std::copy(other.begin(), other.end(), data_);
+    return *this;
 }
 
-template<class T, class Allocator>
+template <class T, class Allocator>
+Vector<T, Allocator> &Vector<T, Allocator>::operator=(Vector &&other) noexcept {
+    if (*this == other) {
+        return *this;
+    }
+    size_ = other.size();
+    capacity_ = other.capacity();
+    data_ = other.data_;
+    other.data_ = nullptr;
+    return *this;
+}
+
+template <class T, class Allocator>
+Vector<T, Allocator>::Vector(const std::initializer_list<T> &init) {
+    size_ = init.size();
+    capacity_ = size_ * 2;
+    data_ = allocator_.allocate(capacity_);
+    std::copy(init.begin(), init.end(), data_);
+}
+
+template <class T, class Allocator>
 Vector<T, Allocator>::~Vector() {
-  if (data_ != nullptr) {
-    for (size_t i = 0; i < size(); ++i) {
-      allocator_.destroy(&data_[i]);
+    if (data_) {
+        for (size_t i = 0; i < size_; ++i) {
+            data_[i].~T();
+        }
+        if (capacity_ > 0) {
+            allocator_.deallocate(data_, capacity_);
+        }
+        data_ = nullptr;
+    }
+}
+
+template <class T, class Allocator>
+T &Vector<T, Allocator>::operator[](size_t pos) {
+    if (pos >= size_) {
+        throw std::out_of_range("Out of range.");
+    }
+    return data_[pos];
+}
+
+template <class T, class Allocator>
+const T &Vector<T, Allocator>::operator[](size_t pos) const {
+    if (pos >= size_) {
+        throw std::out_of_range("Out of range.");
+    }
+    return data_[pos];
+}
+
+
+template <class T, class Allocator>
+T *Vector<T, Allocator>::begin() noexcept {
+    return data_;
+}
+
+template <class T, class Allocator>
+const T *Vector<T, Allocator>::begin() const noexcept {
+    return data_;
+}
+
+template <class T, class Allocator>
+T *Vector<T, Allocator>::end() noexcept {
+    return data_ + size_;
+}
+
+template <class T, class Allocator>
+const T *Vector<T, Allocator>::end() const noexcept {
+    return data_ + size_;
+}
+
+template <class T, class Allocator>
+bool Vector<T, Allocator>::empty() const {
+    return size_ == 0;
+}
+
+template <class T, class Allocator>
+size_t Vector<T, Allocator>::size() const {
+    return size_;
+}
+
+template <class T, class Allocator>
+size_t Vector<T, Allocator>::capacity() const {
+    return capacity_;
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::reserve(size_t new_cap) {
+    if (new_cap > capacity_) {
+        reallocate(new_cap);
+    }
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::clear() {
+    for (size_t i = 0; i < size_; i++) {
+        data_[i].~T();
+    }
+    size_ = 0;
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::push_back(const T &item) {
+    if (capacity_ == 0) {
+        reallocate(1);
+    } else if (size_ == capacity_) {
+        reallocate(capacity_ * 2);
+    }
+    data_[size_] = item;
+    ++size_;
+}
+
+template <class T, class Allocator>
+template <class... Args>
+T *Vector<T, Allocator>::emplace(const T *pos, Args &&...args) {
+    size_t index = pos - data_;
+    if (index > size_) {
+        throw std::out_of_range("Out of range.");
+    }
+    if (capacity_ == 0) {
+        reallocate(1);
+    } else if (size_ == capacity_) {
+        reallocate(capacity_ * 2);
+    }
+    T* it = data_ + index;
+    std::move(it, data_ + size_, it + 1);
+    *it = T(args...);
+    ++size_;
+    return it;
+}
+
+template <class T, class Allocator>
+template <class... Args>
+void Vector<T, Allocator>::emplace_back(Args &&...args) {
+    emplace(data_ + size_, std::forward<Args>(args)...);
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::pop_back() {
+    if (size_ > 0) {
+        data_[size_ - 1].~T();
+        --size_;
+    }
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::resize(size_t count) {
+    resize(count, T());
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::resize(size_t count, const T &value) {
+    if (count > size_) {
+        reserve(count * 2);
+        for (size_t i = size_; i < count; ++i) {
+            data_[i] = value;
+        }
+    } else if (count < size()) {
+        for (size_t i = count; i < size_; ++i) {
+            data_[i].~T();
+        }
+    }
+    size_ = count;
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::print() const {
+    for (size_t i = 0; i < size_; ++i) {
+        std::cout << data_[i] << " ";
+    }
+}
+
+template <class T, class Allocator>
+void Vector<T, Allocator>::reallocate(size_t newCapacity) {
+    T *newData = allocator_.allocate(newCapacity);
+    for (size_t i = 0; i < size_; ++i) {
+        newData[i] = std::move(data_[i]);
     }
     if (capacity_ > 0) {
-      allocator_.deallocate(data(), capacity());
+        allocator_.deallocate(data_, capacity_);
     }
-    data_ = nullptr;
-  }
+    data_ = newData;
+    capacity_ = newCapacity;
 }
 
-template<class T, class Allocator>
-Vector<T, Allocator> &Vector<T, Allocator>::operator=(const Vector &other) {
-  if (this == other) {
-    return *this;
-  }
-  size_ = other.size();
-  capacity_ = other.capacity();
-  data_ = allocator_.allocate(capacity());
-  std::copy(other.begin(), other.end(), data());
-  return *this;
-}
-
-template<class T, class Allocator>
-T &Vector<T, Allocator>::at(size_t pos) {
-  if (pos < size()) {
-    return data_[pos];
-  }
-  throw std::out_of_range("position is out of range");
-}
-
-template<class T, class Allocator>
-const T &Vector<T, Allocator>::at(size_t pos) const {
-  if (pos < size()) {
-    return data_[pos];
-  }
-  throw std::out_of_range("position is out of range");
-}
-
-template<class T, class Allocator>
-T &Vector<T, Allocator>::operator[](size_t pos) {
-  return data_[pos];
-}
-
-template<class T, class Allocator>
-const T &Vector<T, Allocator>::operator[](size_t pos) const {
-  return data_[pos];
-}
-
-template<class T, class Allocator>
-T &Vector<T, Allocator>::back() {
-  return data_[size() - 1];
-}
-
-template<class T, class Allocator>
-const T &Vector<T, Allocator>::back() const {
-  return data_[size() - 1];
-}
-
-template<class T, class Allocator>
-T *Vector<T, Allocator>::data() noexcept {
-  return data_;
-}
-
-template<class T, class Allocator>
-const T *Vector<T, Allocator>::data() const noexcept {
-  return data_;
-}
-
-template<class T, class Allocator>
-T* Vector<T, Allocator>::begin() noexcept {
-  return data_;
-}
-
-template<class T, class Allocator>
-const T* Vector<T, Allocator>::begin() const noexcept {
-  return data_;
-}
-
-template<class T, class Allocator>
-T* Vector<T, Allocator>::end() noexcept {
-  return begin() + size_;
-}
-
-template<class T, class Allocator>
-const T* Vector<T, Allocator>::end() const noexcept {
-  return begin() + size_;
-}
-
-template<class T, class Allocator>
-bool Vector<T, Allocator>::empty() const {
-  return size() == 0;
-}
-
-template<class T, class Allocator>
-size_t Vector<T, Allocator>::size() const {
-  return size_;
-}
-
-template<class T, class Allocator>
-size_t Vector<T, Allocator>::capacity() const {
-  return capacity_;
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::reserve(size_t new_cap) {
-  if (new_cap > capacity()) {
-    reallocate(new_cap);
-    capacity_ = new_cap;
-  }
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::shrink_to_fit() {
-  if (capacity() > size()) {
-    for (size_t i = size(); i < capacity(); ++i) {
-      allocator_.destroy(data() + i);
-    }
-    if (capacity() - size() > 0) {
-      allocator_.deallocate(data() + size(), capacity() - size());
-    }
-    capacity_ = size();
-  }
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::clear() {
-  for (size_t i = 0; i < size(); i++) {
-    allocator_.destroy(data() + i);
-  }
-  size_ = 0;
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::push_back(const T &item) {
-  if (size() == capacity()) {
-    reallocate(capacity() + 1);
-  }
-  allocator_.construct(end(), item);
-  ++size_;
-}
-
-template<class T, class Allocator>
-template<class... Args>
-T *Vector<T, Allocator>::emplace(const T *pos, Args &&... args) {
-  const auto index = pos - begin();
-  if (index < 0 || index > size()) {
-    throw std::out_of_range("Insert index is out of range");
-  }
-  if (size() == capacity()) {
-    reallocate(capacity() + 1);
-  }
-  T *it = &data_[index];
-  std::move(it, end(), it + 1);
-  allocator_.construct(it, args...);
-  ++size_;
-  return it;
-}
-
-template<class T, class Allocator>
-template<class... Args>
-void Vector<T, Allocator>::emplace_back(Args &&... args) {
-  emplace(end(), std::forward<Args>(args)...);
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::pop_back() {
-  if (size() > 0) {
-    allocator_.destroy(end() - 1);
-    --size_;
-  }
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::resize(size_t count) {
-  resize(count, T());
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::resize(size_t count, const T &value) {
-  if (count > capacity()) {
-    this->reserve(count);
-    for (size_t i = size(); i < count; ++i) {
-      allocator_.construct(data() + i, value);
-    }
-  } else if (count < size()) {
-    for (size_t i = count; i < size(); ++i) {
-      allocator_.destroy(data() + i);
-    }
-  }
-  size_ = count;
-}
-
-template<class T, class Allocator>
-[[nodiscard]] size_t Vector<T, Allocator>::calculateCapacity(size_t newSize) const {
-  std::size_t newCapacity;
-  if (newSize < capacity()) {
-    newCapacity = 1;
-  } else if (capacity()){
-    newCapacity = capacity();
-  } else {
-    newCapacity = 1;
-  }
-  while (newCapacity < newSize) {
-    if (newCapacity >= std::numeric_limits<size_t>::max() - newCapacity) {
-      return std::numeric_limits<size_t>::max();
-    }
-    newCapacity *= 2;
-  }
-  return newCapacity;
-}
-
-template<class T, class Allocator>
-void Vector<T, Allocator>::reallocate(size_t minSize) {
-  const size_t newCapacity = calculateCapacity(minSize);
-  T *newData = allocator_.allocate(newCapacity);
-  for (size_t i = 0; i < size(); i++) {
-    allocator_.construct(&newData[i], std::move(data_[i]));
-    allocator_.destroy(&data_[i]);
-  }
-  if (capacity_ > 0) {
-    allocator_.deallocate(data(), capacity());
-  }
-  data_ = newData;
-  capacity_ = newCapacity;
-}
-
-template<class T, class Allocator>
+template <class T, class Allocator>
 bool operator==(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs) {
-  if (lhs.size() != rhs.size()) {
-    return false;
-  }
-  for (size_t i = 0; i < lhs.size(); ++i) {
-    if (lhs.at(i) != rhs.at(i)) {
-      return false;
+    if (lhs.size_ != rhs.size_) {
+        return false;
     }
-  }
-  return true;
+    for (size_t i = 0; i < lhs.size_; ++i) {
+        if (lhs[i] != rhs[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
-template<class T, class Allocator>
+template <class T, class Allocator>
 bool operator!=(const Vector<T, Allocator> &lhs, const Vector<T, Allocator> &rhs) {
-  return !(lhs == rhs);
+    return !(lhs == rhs);
 }
