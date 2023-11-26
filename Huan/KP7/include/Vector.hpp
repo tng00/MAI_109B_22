a5b180ba8fccefac0301e7abfb199faf9209a882
tng00 <80770913+tng00@users.noreply.github.com>
2023-11-27 02:49:31 +0300

    Update Vector.hpp

--- Huan/KP7/include/Vector.hpp
+++ Huan/KP7/include/Vector.hpp
@@ -1,59 +1,48 @@
-#ifndef KP_7_INCLUDE_VECTOR_HPP_
-#define KP_7_INCLUDE_VECTOR_HPP_
+#pragma once
 #include <iostream>
 #include <limits>
 
-template<class T, class Allocator = std::allocator<T>>
+template <class T, class Allocator = std::allocator<T>>
 class Vector {
- public:
-  Vector();
-  explicit Vector(size_t count, const T &value = T());
-  Vector(const Vector &other);
-  Vector(Vector &&other) noexcept;
-  Vector(const std::initializer_list<T>& init);
-  ~Vector();
-  Vector &operator=(const Vector &other);
+public:
+    Vector() = default;
+    Vector(size_t count, const T &value = T());
+    Vector(const Vector &other);
+    Vector(Vector &&other) noexcept;
+    Vector &operator=(const Vector &other);
+    Vector &operator=(Vector &&other) noexcept;
+    Vector(const std::initializer_list<T> &init);
+    ~Vector();
 
-  T &at(size_t pos);
-  const T &at(size_t pos) const;
-  T &operator[](size_t pos);
-  const T &operator[](size_t pos) const;
-  T &back();
-  const T &back() const;
-  T* begin() noexcept;
-  const T* begin() const noexcept;
-  T* end() noexcept;
-  const T* end() const noexcept;
+    T &operator[](size_t pos);
+    const T &operator[](size_t pos) const;
+    T *begin() noexcept;
+    const T *begin() const noexcept;
+    T *end() noexcept;
+    const T *end() const noexcept;
 
-  [[nodiscard]] bool empty() const;
-  [[nodiscard]] size_t size() const;
-  [[nodiscard]] size_t capacity() const;
+    [[nodiscard]] bool empty() const;
+    [[nodiscard]] size_t size() const;
+    [[nodiscard]] size_t capacity() const;
 
-  void reserve(size_t new_cap);
-  void shrink_to_fit();
-  void clear();
+    void reserve(size_t new_cap);
+    void clear();
 
-  void push_back(const T &item);
-  template<class... Args>
-  T* emplace(const T* pos, Args &&... args);
-  template<class... Args>
-  void emplace_back(Args &&... args);
-  void pop_back();
-  void resize(size_t count);
-  void resize(size_t count, const T &value);
+    void push_back(const T &item);
+    template <class... Args>
+    T *emplace(const T *pos, Args &&...args);
+    template <class... Args>
+    void emplace_back(Args &&...args);
+    void pop_back();
+    void resize(size_t count);
+    void resize(size_t count, const T &value);
 
- private:
-  size_t size_ = 0;
-  size_t capacity_ = 0;
-  T *data_;
-  Allocator allocator_;
+    void print() const;
 
-  T *data() noexcept;
-  const T *data() const noexcept;
-  [[nodiscard]] size_t calculateCapacity(size_t newSize) const;
-  void reallocate(size_t minSize);
+private:
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+    T *data_ = nullptr;
+    Allocator allocator_;
+    void reallocate(size_t minSize);
 };
-
-#include "../src/Vector.cpp"
-
-#endif //KP_7_INCLUDE_VECTOR_HPP_
